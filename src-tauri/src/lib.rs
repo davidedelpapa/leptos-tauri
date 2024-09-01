713d607ac9de1aac872d805aa127525747de457c
Davide Del Papa <davidedelpapa@yahoo.com>
2024-09-01 12:18:33 +0200

    Commit tutorial v1.2

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,19 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn increase(count: i32) -> i32 {
+    count + 1
+}
+
+#[tauri::command]
+fn decrease(count: i32) -> i32 {
+    count - 1
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![increase, decrease])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/app.rs
+++ src/app.rs
@@ -1,18 +1,46 @@
 // src/app.rs
+use leptos::leptos_dom::ev::MouseEvent;
 use leptos::*;
+use serde::{Deserialize, Serialize};
+use serde_wasm_bindgen::to_value;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = ["window", "__TAURI__", "core"])]
+    async fn invoke(cmd: &str, args: JsValue) -> JsValue;
+}
+
+#[derive(Serialize, Deserialize)]
+struct CounterArgs {
+    count: i32,
+}
 
 #[component]
 pub fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
-    // added
+    let increase_me = move |ev: MouseEvent| {
+        ev.prevent_default();
+        spawn_local(async move {
+            let count = count.get_untracked();
+            let args = to_value(&CounterArgs { count }).unwrap();
+            let new_value = invoke("increase", args).await.as_f64().unwrap();
+            set_count.set(new_value as i32);
+        });
+    };
+    let decrease_me = move |ev: MouseEvent| {
+        ev.prevent_default();
+        spawn_local(async move {
+            let count = count.get_untracked();
+            let args = to_value(&CounterArgs { count }).unwrap();
+            let new_value = invoke("decrease", args).await.as_f64().unwrap();
+            set_count.set(new_value as i32);
+        });
+    };
     view! {
         <div>
-            <button on:click=move |_| {
-                set_count.update(|n| *n += 1);
-            }>"+1"</button>
-            <button on:click=move |_| {
-                set_count.update(|n| *n -= 1);
-            }>"-1"</button>
+            <button on:click=increase_me>"+1"</button>
+            <button on:click=decrease_me>"-1"</button>
             <p class:red=move || count() < 0>"Counter: "{count}</p>
         </div>
     }
